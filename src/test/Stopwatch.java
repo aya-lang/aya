7252edbfb28171203d73fb5b96db8ba08f030240
nicholaskapaul <nicholaskapaul@gmail.com>
2016-12-26 00:34:44 -0500

    Add stopwatch class for testing

---
+++ src/test/Stopwatch.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Stopwatch {
+	long _start;
+	long _elapsed;
+	
+	public Stopwatch() {}
+	
+	public void start() {
+		_start = System.currentTimeMillis();
+	}
+	
+	public void stop() {
+		_elapsed = System.currentTimeMillis() - _start;
+	}
+	
+	public double secs() {
+		return ((double)_elapsed/1000000000);
+	}
+}
