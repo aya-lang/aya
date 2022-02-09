c20a2f9e8c644a66d5721af2000efa45de08a6d6
nick-paul <npaul@ltu.edu>
2022-02-09 01:04:57 -0500

    Windows path compatibility fixes

--- src/aya/util/FileUtils.java
+++ src/aya/util/FileUtils.java
@@ -2,8 +2,11 @@ package aya.util;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 import aya.AyaPrefs;
 
@@ -17,7 +20,7 @@ public class FileUtils {
 		
 		try {
 			String line;
-			br = new BufferedReader(new FileReader(file));
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
 			while((line = br.readLine()) != null) {
 				sb.append(line+'\n');
 			}
