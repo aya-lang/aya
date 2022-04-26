cd37ef846296d5358f0b7f213c28a404bb22d2cc
Nick Paul <nicholas.paul@soartech.com>
2022-04-25 22:08:51 -0400

    Fix error message string in IndexError to display the proper index value if negative

--- src/aya/exceptions/runtime/IndexError.java
+++ src/aya/exceptions/runtime/IndexError.java
@@ -3,14 +3,36 @@ package aya.exceptions.runtime;
 import aya.obj.Obj;
 import aya.obj.dict.Dict;
 import aya.obj.list.List;
+import aya.obj.number.Num;
 import aya.obj.symbol.Symbol;
 import aya.obj.symbol.SymbolConstants;
+import aya.util.Casting;
 
 @SuppressWarnings("serial")
 public class IndexError extends InternalAyaRuntimeException {
 
+	// List.index converts a negative index to a backward index by simply adding the length.
+	// If the magnitude of the negative index is larger than the list we will get an
+	// out of bounds error but the index in the error will be off by the length of the list
+	// This function allows us to undo it before sending the error
+	// For example (without this function):
+	//  aya> [1 2 3] :20 I
+	//  Error: index out of bounds: -17
+	// Since List.index([1 2 3], :20) returns -20 + 3 => -17
+	public static Obj resolveNegativeListIndex(List l, Obj index) {
+		if (index.isa(Obj.NUMBER)) {
+			int idx = Casting.asNumber(index).toInt();
+			if (idx < 0) {
+				idx -= l.length();
+			}
+			return Num.fromInt(idx);
+		} else {
+			return index;
+		}
+	}
+	
 	public IndexError(List l, Obj index) {
-		super(SymbolConstants.INDEX_ERR, "Invalid index " + index.repr() + " for list " + l.repr());
+		super(SymbolConstants.INDEX_ERR, "Invalid index " + resolveNegativeListIndex(l, index).repr() + " for list " + l.repr());
 	}
 	
 	public IndexError(Dict dict, Obj index, String message) {
