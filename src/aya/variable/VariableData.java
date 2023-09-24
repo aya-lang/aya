97d1be822218a05c3a8e40af7f4b33a6444ebdec
nick-paul <npaul@ltu.edu>
2023-09-24 19:36:24 -0400

    Move nil to ayarc

--- src/aya/variable/VariableData.java
+++ src/aya/variable/VariableData.java
@@ -63,25 +63,6 @@ public class VariableData {
 		initGlobals(aya);
 	}
 	
-	private void initNil(Aya aya) {
-		Dict nil_meta = new Dict();
-		nil_meta.set(SymbolConstants.KEYVAR_TYPE, Aya.getInstance().getSymbols().getSymbol("__nil"));
-		List nil_str = List.fromString("nil");
-		nil_meta.set(SymbolConstants.KEYVAR_STR, nil_str);
-		nil_meta.set(SymbolConstants.KEYVAR_REPR, nil_str);
-		nil_meta.set(SymbolConstants.KEYVAR_PUSHSELF, Num.ONE);
-		
-		Block nil_eq = null;
-		try {
-			nil_eq = Parser.compile(new SourceString("; :T ::__nil =", "<nil_eq>"), aya);
-		} catch (ParserException e) {
-			throw new RuntimeException(e);
-		}
-		nil_meta.set(SymbolConstants.KEYVAR_EQ, nil_eq);
-		
-		OBJ_NIL.setMetaTable(nil_meta);
-	}
-	
 	public void initGlobals(Aya aya) {
 		Dict globals = new Dict();
 		
@@ -105,9 +86,6 @@ public class VariableData {
 		BUILTINS.set(SymbolConstants.NUM,   OBJ_NUM);
 		BUILTINS.set(SymbolConstants.STR,   OBJ_STR);
 		
-		initNil(aya);
-		globals.set(SymbolConstants.NIL, OBJ_NIL);
-
 		_var_sets.add(new Scope(globals));
 	}
 	
