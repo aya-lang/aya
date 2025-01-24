0117f506fa1fe7b5588104e56b440fe0dc37d699
BlazingTwist <blazingtwist1@gmail.com>
2025-01-24 16:12:17 +0100

    Fix classloader and parser handling for libraries, add tests - resolves a ClassNotFoundException when libraries contain multiple classes - resolves a ParserException when using libraries in the same file that imports them - adds automated tests for compiling and using libraries

--- src/aya/instruction/named/NamedOperatorInstruction.java
+++ src/aya/instruction/named/NamedOperatorInstruction.java
@@ -1,27 +1,44 @@
 package aya.instruction.named;
 
 import aya.ReprStream;
+import aya.StaticData;
 import aya.eval.BlockEvaluator;
+import aya.exceptions.runtime.InternalAyaRuntimeException;
 import aya.instruction.Instruction;
+import aya.obj.symbol.SymbolConstants;
 import aya.parser.SourceStringRef;
 
 public class NamedOperatorInstruction extends Instruction {
 
-	private NamedOperator op;
-	
-	public NamedOperatorInstruction(SourceStringRef source, NamedOperator op) {
-		super(source);
-		this.op = op;
-	}
+    private final String opName;
+    private NamedOperator op = null;
 
-	@Override
-	public void execute(BlockEvaluator blockEvaluator) {
-		this.op.execute(blockEvaluator);
-		
-	}
+    public NamedOperatorInstruction(SourceStringRef source, String opName) {
+        super(source);
+        this.opName = opName;
+    }
 
-	@Override
-	public ReprStream repr(ReprStream stream) {
-		return this.op.repr(stream);
-	}
+    private void loadOp() {
+        if (op != null) {
+            return;
+        }
+
+        op = StaticData.getInstance().getNamedInstruction(opName);
+        if (op == null) {
+            throw new InternalAyaRuntimeException(SymbolConstants.NOT_AN_OP_ERROR, "Named instruction :(" + opName + ") does not exist");
+        }
+    }
+
+    @Override
+    public void execute(BlockEvaluator blockEvaluator) {
+        this.loadOp();
+        op.execute(blockEvaluator);
+    }
+
+    @Override
+    public ReprStream repr(ReprStream stream) {
+        // repr doesn't need to load the OP-instance, we already know the opName.
+        stream.print(":(" + opName + ")");
+        return stream;
+    }
 }
